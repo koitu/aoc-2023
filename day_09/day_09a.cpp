51eb73a84c18470ebb70a35c5763dcb7f0f5fdd3
Andrew Wang <someone.zip@gmail.com>
2023-12-12 13:36:05 -0500

    day 9 done

--- day_09/day_09a.cpp
+++ day_09/day_09a.cpp
@@ -1,3 +1,60 @@
-//
-// Created by rmus-deployment on 12/12/23.
-//
+#include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+std::vector<double> num_split(std::string s) {
+    std::vector<double> f;
+    size_t pos;
+
+    while((pos = s.find(" ")) != std::string::npos) {
+        f.push_back(stod(s.substr(0, pos)));
+        s.erase(0, pos + 1);
+    }
+    f.push_back(stod(s));
+
+    return f;
+}
+
+int main(int argc, char *argv[]) {
+    std::string input = "./input.txt";
+
+    std::fstream file(input);
+    std::string line;
+
+    getline(file, line);  // assume we always have at least one line
+    std::string temp = line;
+
+    std::vector<double> lst = num_split(temp);
+    const int n = lst.size();  // assume we always have at least one number
+
+    std::vector<double> l;
+    // Unisolvence thm: given n data pairs (x_i, y_i) with distinct x_i there is a unique polynomial of degree <= n-1
+    //  that interpolates the data
+
+    // https://en.wikipedia.org/wiki/Lagrange_polynomial
+    for (double i = 0; i < n; i++) {
+        double h = 1;
+
+        for (double j = 0; j < n; j++) {
+            if (i != j) {
+                h *= (n - j) / (i - j);
+            }
+        }
+
+        l.push_back(h);
+    }
+
+    long long res = 0;
+    do {
+        lst = num_split(line);
+        for (int i = 0; i < n; i++) {
+            res += std::llround(l[i] * lst[i]);
+        }
+    } while(getline(file, line));
+
+    std::cout << res << std::endl;  // 2008960228
+
+    return 0;
+}
--- day_09/day_09b.cpp
+++ day_09/day_09b.cpp
@@ -1,3 +1,52 @@
-//
-// Created by rmus-deployment on 12/12/23.
-//
+#include <cmath>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+std::vector<double> num_split(const std::string s) {
+    std::string ss = s;
+    std::vector<double> l;
+    size_t pos;
+
+    while ((pos = ss.find(" ")) != std::string::npos) {
+        l.push_back(stod(ss.substr(0, pos)));
+        ss.erase(0, pos + 1);
+    }
+    l.push_back((stod(ss)));
+
+    return l;
+}
+
+int main(int argc, char* argv[]) {
+    std::string input = "./input.txt";
+
+    std::fstream file(input);
+    std::string line;
+
+    getline(file, line);
+    std::vector<double> lst = num_split(line);
+    const int n = lst.size();
+
+    std::vector<double> l;
+    for (double i = 1; i <= n; i++) {
+        double t = 1;
+
+        for (double j = 1; j <= n; j++) {
+            if (i != j) {
+                t *= -j / (i - j);
+            }
+        }
+        l.push_back(t);
+    }
+
+    long long res = 0;
+    do {
+        lst = num_split(line);
+        for (int i = 0; i < n; i++) {
+            res += llround(lst[i] * l[i]);
+        }
+    } while (getline(file, line));
+
+    std::cout << res << std::endl;  // 1097
+}
